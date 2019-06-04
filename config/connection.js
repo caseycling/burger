9359a6744adac4457a7bcc0fae425e764bed5a2a
Casey <caseyclinga@gmail.com>
2019-06-04 12:52:11 -0700

    Heroku connection code added

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 //Set up connection to MySQL
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "$3ndNudes",
     database: "burgers_db",
-});
+    });
+}
 
 //Fire connection
 connection.connect(function(err) {
