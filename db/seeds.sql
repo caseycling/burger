5b5f706b22ce39f0a1f73f3bf2bee83166691c69
Casey <caseyclinga@gmail.com>
2019-06-03 19:03:52 -0700

    Populated seeds with two more entries

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
-INSERT INTO burgers (name) VALUES ("Double bacon-cheeseburger")
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double bacon-cheeseburger", 0)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken bacon ranch burger", 0)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black-bean burger", 0)
