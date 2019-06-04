96b9cae834fb336923b17bb717daa4043047bb8c
Casey <caseyclinga@gmail.com>
2019-06-03 21:47:50 -0700

    Fixed port for connection to MySQL

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: PORT,
+    port: 3306,
     user: "root",
     password: "$3ndNudes",
     database: "burgers_db",
