2456dee48f8d8ec6295c2905b6d76cf010789ea4
Casey <caseyclinga@gmail.com>
2019-06-04 12:33:25 -0700

    Deleted unnecesary brackets

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -8,8 +8,8 @@ var burger = require("../models/burger")
 //Routes
 
 //Root route will get all burgers in the burgers table
-router.get("/", function(req, res) {
-    burger.all(function(data) {
+router.get("/", function (req, res) {
+    burger.all(function (data) {
         var burgObj = {
             //"burgers" keyword here will be used to pass data to index.handlebars
             burgers: data
@@ -21,20 +21,32 @@ router.get("/", function(req, res) {
 })
 
 //This route will add burgers to the database
-router.post("/burgers", function(req, res) {
+router.post("/burgers", function (req, res) {
     burger.create([
         "burger_name", "devoured"
     ], [
-        req.body.burgName, 0
-    ], function(result) {
-        res.json({ d: result.insertId})
-    })
+            req.body.burgName, 0
+        ], function (result) {
+            res.json({ d: result.insertId })
+        })
 })
 
 //This route will change devoured value after user clicks the devour button
-router.put("/burgers/:id", function(req, res) {
+router.put("/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id
 
-}) 
+    console.log("Burger eaten:", condition)
+    //Change status of burger to devoured
+    burger.update({
+        devoured: 1
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end()
+        }
+    })
+})
 
 //Export routes via router
 module.exports = router
