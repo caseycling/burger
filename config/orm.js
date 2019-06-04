01c1097faf2e5c65fb57f7d264a479dde2bf3ced
Casey <caseyclinga@gmail.com>
2019-06-03 19:09:41 -0700

    Created frame for and exported the orm

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,16 @@
 //Import the connection from connection.js
 var connection = require('./connection')
+
+var orm = {
+    selectAll: function() {
+        
+    },
+    insetOne: function() {
+
+    },
+    updateOne: function() {
+
+    }
+}
+
+module.exports = orm;
